5ab15a84426000f812880b2bdeba263e05dd47e7
Yusif Yusifov <yusif@yay.com>
2018-09-16 02:48:02 +0400

    Little fix

--- mcron.py
+++ mcron.py
@@ -10,7 +10,6 @@ p = poster()
 try:
     cursor = _conn.telegram.medium_users.find({"mstatus":1},{"_id":1,"medium_id":1})
     for user in cursor:
-        print(user["medium_id"])
         mp = medium_parser([user["medium_id"],user["_id"]])
         result = mp.get_new_pubs()
         if len(result) > 0:
